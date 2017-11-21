8a8f8a92a9615ed6298439ccf146791ae7873dd0
Kyle Barrett <kyle@squaretwo.co>
2017-11-21 10:58:18 -0800

    Explicitly enable should expectations.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,9 @@ require 'rest_client'
 require 'hollar'
 require 'hollar/errors/authentication_error'
 require 'test_content'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = [:should, :expect]
+  end
+end
